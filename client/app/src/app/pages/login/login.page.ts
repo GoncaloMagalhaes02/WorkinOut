f8a278023eeaedabcbf46ea6b42d8a73936d9b27
Guilherme Magalhaes <gui.oamagalhaes@gmail.com>
2025-05-21 14:59:19 +0100

    login

--- client/app/src/app/pages/login/login.page.ts
+++ client/app/src/app/pages/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +10,52 @@ import { Component, OnInit } from '@angular/core';
   standalone: false,
 })
 export class LoginPage implements OnInit {
-  constructor() {}
+  name: string = '';
+  password: string = '';
+
+
+
+  constructor( private http: HttpClient, private router: Router, private toastController: ToastController) {}
 
   ngOnInit() {}
+
+  async loginUser() {
+    const formData = {
+      name: this.name,
+      password: this.password,
+    };
+
+    this.http.post('http://localhost:3000/users/login', formData).subscribe({
+      next: async (response) => {
+        console.log('Login bem-sucedido', response);
+        await this.toastSuccess();
+        this.router.navigate(['/home']);
+      },
+      error: async (error) => {
+        console.error('Erro ao fazer login', error);
+        await this.toastError();
+      },
+    });
+  }
+
+  async toastSuccess() {
+    const toast = await this.toastController.create({
+      message: 'Login bem-sucedido',
+      duration: 2000,
+      position: 'bottom',
+      color: 'success',
+    });
+    toast.present();
+  }
+
+  async toastError() {
+    const toast = await this.toastController.create({
+      message: 'Erro ao fazer login',
+      duration: 2000,
+      position: 'bottom',
+      color: 'danger',
+    });
+    toast.present();
+  }
+
 }
